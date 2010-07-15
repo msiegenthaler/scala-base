78172ee3e1f74e5a764b60f9cde92aa0986bb270
msiegenthaler <msiegenthaler@gmx.net>
2010-07-16 00:37:03 +0200

    rename ProcessCps2 to ProcessCps remove tests for messagequeue

--- src/test/scala/process/cps/MessageQueueReadWriteLockSpec.scala
+++
@@ -1,9 +0,0 @@
-package ch.inventsoft.scalabase.process.cps
-
-import Process._
-import collection.mutable.{Buffer,ListBuffer}
-
-class MessageQueueReadWriteLockSpec extends MessageQueueSpec  {
-  override def makeQueue[A] = new MessageQueueReadWriteLock[A]
-  override def name = "MessageQueueReadWriteLock"
-}
--- src/test/scala/process/cps/MessageQueueSyncSpec.scala
+++
@@ -1,9 +0,0 @@
-package ch.inventsoft.scalabase.process.cps
-
-import Process._
-import collection.mutable.{Buffer,ListBuffer}
-
-class MessageQueueSyncSpec extends MessageQueueSpec  {
-  override def makeQueue[A] = new MessageQueueSync[A]
-  override def name = "MessageQueueSync"
-}
