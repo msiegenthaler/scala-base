5257f1db8b8379f371f29949ad3ecc37c75b8a92
msiegenthaler <msiegenthaler@gmx.net>
2010-08-26 20:07:52 +0200

    complete context (not just direct parent) and namespace-support.

--- src/main/scala/io/XMLChunkSource.scala
+++ src/main/scala/io/XMLChunkSource.scala
@@ -31,75 +31,129 @@ trait XmlChunker {
   def push(data: Iterable[Char]): XmlChunker
   def +(data: Iterable[Char]) = push(data)
 
-  /** The root-element (without children) */
-  def root: Option[Elem]
-  /** true if inside a valid root-element (root.isDefined) */
-  def insideXml = root.isDefined
-
-  /** Chunks that were discovered in the last (last discovered is first in list)*/
-  def chunks: List[Iterable[Char]]
-  def chunksAsString: List[String] = chunks.map(it => new String(it.toArray))
-  //TODO chunks as xml (mapped)
+  /** Chunks that were discovered (earliest discovered is first in list)*/
+  def chunks: List[XmlChunk]
   def hasChunks = chunks.nonEmpty
-
   /** Consume all the chunks (free the memory) */
-  def consumed: XmlChunker
+  def consumeAll: XmlChunker
+  def consumeChunk: (Option[XmlChunk], XmlChunker)
 }
+trait XmlChunk {
+  /** the content of this chunk as character array */
+  def chars: Iterable[Char]
+  def string = new String(chars.toArray)
+  /** this chunk as xml-data (if parsable) */
+  lazy val xml: Option[Elem] = {
+    try {
+      val content = chars.toArray
+      val nsdecls = context.map(_.scope).map(" "+_.buildString(null)).getOrElse("")
+      val str = new StringBuilder(nsdecls.length+content.length+8).append("<a").append(nsdecls).append(">").appendAll(content).append("</a>")
+      val reader = new java.io.StringReader(str.toString)
+      val elem = XML.load(reader)
+      elem.child.headOption.asInstanceOf[Option[Elem]]
+    } catch {
+      case e: Exception => None
+    }
+  }
+  def context: Option[Elem]
+  override def toString = string
+}
+
 object XmlChunker extends Log {
-  def apply(depth: Int = 1): XmlChunker = LookingForElement(depth, None, Nil, Nil, 0)
+  def apply(depth: Int = 1) = new XmlChunkers(depth).init
 
   //TODO cdata handling
   //TODO max size of parsed stuff (1Mb or so): mostly collected and elementData
 
-  private case class LookingForElement(rootDepth: Int, root: Option[Elem], chunks: List[Iterable[Char]], collected: Iterable[Char], depth: Int) extends XmlChunker {
-    override def push(data: Iterable[Char]) = {
-      val (h,t) = data.span(_ != '<')
-      val nc = collected ++ h
-      if (t.nonEmpty) InElementTag(rootDepth, root, chunks, nc, depth, Nil).push(t)
-      else copy(collected = nc)
+  private class XmlChunkImpl(val chars: Iterable[Char], val context: Option[Elem]) extends XmlChunk
+
+  private class XmlChunkers(rootDepth: Int) {
+    def init: XmlChunker = LookingForElement(Nil, Nil, Nil, 0)
+
+    private case class LookingForElement(parents: List[Elem], chunks: List[XmlChunk], collected: Iterable[Char], depth: Int) extends XmlChunker {
+      override def push(data: Iterable[Char]) = {
+        val (h,t) = data.span(_ != '<')
+        val nc = collected ++ h
+        if (t.nonEmpty) InElementTag(parents, chunks, nc, depth, Nil).push(t)
+        else copy(collected = nc)
+      }
+      override def consumeAll = copy(chunks = Nil)
+      override def consumeChunk = chunks match {
+        case chunk :: rest => (Some(chunk), copy(chunks = rest))
+        case Nil => (None, this)
+      }
     }
-    override def consumed = copy(chunks=Nil)
-  }
-  private case class InElementTag(rootDepth: Int, root: Option[Elem], chunks: List[Iterable[Char]], collected: Iterable[Char], depth: Int, elementData: Iterable[Char]) extends XmlChunker {
-    override def push(data: Iterable[Char]) = {
-      val (h,t) = data.span(_ != '>')
-      if (t.nonEmpty) {
-        val tail = t.drop(1)
-        val tag = elementData ++ h ++ ">"
-        val chunk = collected ++ tag
-        if (tag.drop(1).head == '/') {
-          //Close of element
-          val next: XmlChunker = {
-            if (depth <= rootDepth) LookingForElement(rootDepth, None, chunks, Nil, (depth-1).max(0))
-            else if (depth==rootDepth+1) {
-              val newChunks = if (chunk.isEmpty) chunks else chunk :: chunks
-              LookingForElement(rootDepth, root, newChunks, Nil, depth-1)
-            } else LookingForElement(rootDepth, root, chunks, chunk, depth-1)
-          }
-          next.push(tail)
-        } else {
-          //Open of element
-          if (tag.takeRight(2).head == '/') {
-            if (depth==rootDepth) LookingForElement(rootDepth, root, chunk :: chunks, Nil, depth).push(tail)
-            else LookingForElement(rootDepth, root, chunks, chunk, depth).push(tail)
+    private case class InElementTag(parents: List[Elem], chunks: List[XmlChunk], collected: Iterable[Char], depth: Int, elementData: Iterable[Char]) extends XmlChunker {
+      override def push(data: Iterable[Char]) = {
+        val (h,t) = data.span(_ != '>')
+        if (t.nonEmpty) {
+          val tail = t.drop(1)
+          val tag = elementData ++ h ++ ">"
+          val chunk = collected ++ tag
+          if (tag.drop(1).head == '/') {
+            //Close of element
+            handleElementClose(tag, chunk).push(tail)
+          } else if (tag.takeRight(2).head == '/') {
+            //Inline close
+            handleElementOpenInlineClose(tag, chunk).push(tail)
           } else {
-            if (depth<rootDepth) {
-              val elementData = tag.dropRight(1) ++ "/>"
-              val reader = new java.io.CharArrayReader(elementData.toArray)
-              try {
-                val xml = XML.load(reader)
-                LookingForElement(rootDepth, Some(xml), chunks, Nil, depth+1).push(tail)
-              } catch {
-                case e: Exception =>
-                  log.info("Invalid XML received (possible root): {}", e)
-                  LookingForElement(rootDepth, None, chunks, Nil, depth).push(tail)
-              }
-            } else LookingForElement(rootDepth, root, chunks, chunk, depth+1).push(tail)
+            //Opening tag
+            handleElementOpen(tag, chunk).push(tail)
           }
+        } else copy(elementData = elementData ++ h)
+      }
+      /** tag = <element> */
+      protected def handleElementOpen(tag: Iterable[Char], chunk: Iterable[Char]): XmlChunker = {
+        if (depth < rootDepth) {
+          parseXml(tag.dropRight(1) ++ "/>") match {
+            case Some(elem) =>
+              LookingForElement(elem :: parents, chunks, Nil, depth+1)
+            case None =>
+              LookingForElement(parents, chunks, Nil, depth)
+          }
+        } else LookingForElement(parents, chunks, chunk, depth+1)
+      }
+      /** tag = <element/> */
+      protected def handleElementOpenInlineClose(tag: Iterable[Char], chunk: Iterable[Char]): XmlChunker = {
+        if (depth == rootDepth) LookingForElement(parents, chunks ::: List(mkChunk(chunk)), Nil, depth)
+        else LookingForElement(parents, chunks, chunk, depth)
+      }
+      /** tag = </element> */
+      protected def handleElementClose(tag: Iterable[Char], chunk: Iterable[Char]): XmlChunker = {
+        if (depth <= rootDepth) {
+          val newDepth = (depth - 1) max 0
+          LookingForElement(parents.drop(1), chunks, Nil, newDepth)
+        } else if (depth == rootDepth+1) {
+          val newChunks = if (chunk.isEmpty) chunks else chunks ::: List(mkChunk(chunk))
+          LookingForElement(parents, newChunks, Nil, depth-1)
+        } else LookingForElement(parents, chunks, chunk, depth-1)
+      }
+
+      protected def parseXml(data: Iterable[Char]): Option[Elem] = {
+        try {
+          val reader = new java.io.CharArrayReader(data.toArray)
+          Some(XML.load(reader))
+        } catch { case e: Exception =>
+          log.debug("Invalid XML received (possible root): {}", e)
+          None
         }
-      } else copy(elementData = elementData ++ h)
+      }
+      protected def mkChunk(data: Iterable[Char]): XmlChunk = {
+        val root = parents match {
+          case value :: Nil => Some(value)
+          case first :: rest =>
+            val elem = rest.foldLeft(first)((child,e) => e.copy(child = child :: Nil))
+            Some(elem)
+          case Nil => None
+        }
+        new XmlChunkImpl(data, root)
+      }
+      override def consumeAll = copy(chunks = Nil)
+      override def consumeChunk = chunks match {
+        case chunk :: rest => (Some(chunk), copy(chunks = rest))
+        case Nil => (None, this)
+      }
     }
-    override def consumed = copy(chunks=Nil)
   }
 
   private val cdataStart = "<![CDATA[".toCharArray
--- src/test/scala/io/XmlChunkSourceSpec.scala
+++ src/test/scala/io/XmlChunkSourceSpec.scala
@@ -16,116 +16,240 @@ class XmlChunkSourceSpec extends ProcessSpec with ShouldMatchers {
       val c = XmlChunker()
       c.chunks should be(Nil)
       c.hasChunks should be(false)
-      c.insideXml should be(false)
+      val (chunk, nc)  = c.consumeChunk
+      chunk should be(None)
+      nc should be(c)
     }
     it("should not return any chunks after processing an empty iterator") {
       val c = XmlChunker() + ""
       c.chunks should be(Nil)
       c.hasChunks should be(false)
-      c.insideXml should be(false)
     }
 
     it("should return no chunks after the root xml element has been partialy opened") {
       val c = XmlChunker() + "<root"
       c.chunks should be(Nil)
       c.hasChunks should be(false)
-      c.insideXml should be(false)
     }
     it("should return no chunks after the root xml element has been opened") {
       val c = XmlChunker() + "<root>"
       c.chunks should be(Nil)
       c.hasChunks should be(false)
-      c.insideXml should be(true)
     }
     it("should return no chunks after the root xml element has been opened and some additional data is received") {
       val c = XmlChunker() + "<root>bla"
       c.chunks should be(Nil)
       c.hasChunks should be(false)
-      c.insideXml should be(true)
     }
     it("should return no chunks on xmls with no content") {
       val c = XmlChunker() + "<root></root>"
       c.chunks should be(Nil)
       c.hasChunks should be(false)
-      c.insideXml should be(false)
     }
     it("should return no chunks on xmls with no content (inline closed)") {
       val c = XmlChunker() + "<root/>"
       c.chunks should be(Nil)
       c.hasChunks should be(false)
-      c.insideXml should be(false)
     }
     it("should return no chunks on xmls with no first level elements") {
       val c = XmlChunker() + "<root>bla</root>"
       c.chunks should be(Nil)
       c.hasChunks should be(false)
-      c.insideXml should be(false)
     }
-    it("should be able to get the root-element if inside") {
-      val c = XmlChunker() + "<root>"
-      c.chunks should be(Nil)
-      c.insideXml should be(true)
-      c.root should be(Some(<root/>))
+    it("should be able to get the root-element for a chunk") {
+      val c = XmlChunker() + "<root><a/>"
+      val (Some(e),nc) = c.consumeChunk
+      e.string should be("<a/>")
+      e.context should be(Some(<root/>))
+      nc.hasChunks should be(false)
     }
-    it("should be able to get the root-element with attribute if inside") {
-      val c = XmlChunker() + "<root a=\"hallo welt\">"
-      c.chunks should be(Nil)
-      c.insideXml should be(true)
-      c.root should be(Some(<root a="hallo welt"/>))
+    it("should be able to get the root-element with attribute for a chunk") {
+      val c = XmlChunker() + "<root a=\"hallo welt\"><a/>"
+      c.consumeChunk match {
+        case (Some(chunk),c) =>
+          chunk.string should be("<a/>")
+          chunk.xml should be(Some(<a/>))
+          chunk.context should be(Some(<root a="hallo welt"/>))
+          c.hasChunks should be(false)
+        case _ => fail
+      }
     }
     it("should detect a simple, empty element") {
       val c = XmlChunker() + "<root><bla></bla></root>"
-      c.chunksAsString should be("<bla></bla>" :: Nil)
-      c.insideXml should be(false)
+      c.consumeChunk match {
+        case (Some(chunk),c) =>
+          chunk.string should be("<bla></bla>")
+          chunk.xml should be(Some(<bla></bla>))
+          chunk.context should be(Some(<root/>))
+          c.hasChunks should be(false)
+        case _ => fail
+      }
     }
     it("should detect a simple, empty element in still open root") {
       val c = XmlChunker() + "<root><bla></bla>"
-      c.chunksAsString should be("<bla></bla>" :: Nil)
-      c.insideXml should be(true)
-      val c2 = c + "</root>"
-      c2.insideXml should be(false)
+      c.consumeChunk match {
+        case (Some(chunk),c) =>
+          chunk.string should be("<bla></bla>")
+          chunk.xml should be(Some(<bla></bla>))
+          chunk.context should be(Some(<root/>))
+          c.hasChunks should be(false)
+        case _ => fail
+      }
     }
     it("should detect a simple, empty element (inline closed)") {
       val c = XmlChunker() + "<root><bla/>"
-      c.chunksAsString should be("<bla/>" :: Nil)
-      c.insideXml should be(true)
+      c.consumeChunk match {
+        case (Some(chunk),c) =>
+          chunk.string should be("<bla/>")
+          chunk.xml should be(Some(<bla/>))
+          chunk.context should be(Some(<root/>))
+          c.hasChunks should be(false)
+        case _ => fail
+      }
     }
     it("should detect a simple, empty element with attributes") {
       val c = XmlChunker() + "<root><bla a=\"b\"></bla>"
-      c.chunksAsString should be("<bla a=\"b\"></bla>" :: Nil)
-      c.insideXml should be(true)
+      c.consumeChunk match {
+        case (Some(chunk),c) =>
+          chunk.string should be("<bla a=\"b\"></bla>")
+          chunk.xml should be(Some(<bla a="b"></bla>))
+          chunk.context should be(Some(<root/>))
+          c.hasChunks should be(false)
+        case _ => fail
+      }
     }
     it("should detect an emtpy namespaced element") {
       val c = XmlChunker() + "<root><x:bla xmlns:x=\"http://myapp\"></x:bla>"
-      c.chunksAsString should be("<x:bla xmlns:x=\"http://myapp\"></x:bla>" :: Nil)
-      c.insideXml should be(true)
+      c.consumeChunk match {
+        case (Some(chunk),c) =>
+          chunk.string should be("<x:bla xmlns:x=\"http://myapp\"></x:bla>")
+          chunk.xml should be(Some(<x:bla xmlns:x="http://myapp"></x:bla>))
+          chunk.context should be(Some(<root/>))
+          c.hasChunks should be(false)
+        case _ => fail
+      }
     }
     it("should detect a simple element with content") {
       val c = XmlChunker() + "<root><bla>mycontent</bla>"
-      c.chunksAsString should be("<bla>mycontent</bla>" :: Nil)
-      c.insideXml should be(true)
+      c.consumeChunk match {
+        case (Some(chunk),c) =>
+          chunk.string should be("<bla>mycontent</bla>")
+          chunk.context should be(Some(<root/>))
+          c.hasChunks should be(false)
+        case _ => fail
+      }
     }
     it("should detect a simple element with element content") {
       val c = XmlChunker() + "<root><bla>my<value>content</value> is <b><i>test</i></b></bla>"
-      c.chunksAsString should be("<bla>my<value>content</value> is <b><i>test</i></b></bla>" :: Nil)
-      c.insideXml should be(true)
+      c.consumeChunk match {
+        case (Some(chunk),c) =>
+          chunk.string should be("<bla>my<value>content</value> is <b><i>test</i></b></bla>")
+          chunk.context should be(Some(<root/>))
+          c.hasChunks should be(false)
+        case _ => fail
+      }
+    }
+    it("should detect multiple elements inside one root") {
+      val c = XmlChunker() + "<root><one/><two>2</two><three/>"
+      c.hasChunks should be(true)
+      c.chunks.size should be(3)
+      val chunk1 :: chunk2 :: chunk3 :: Nil = c.chunks
+      chunk1.string should be("<one/>")
+      chunk1.xml should be(Some(<one/>))
+      chunk1.context should be(Some(<root/>))
+      chunk2.string should be("<two>2</two>")
+      chunk2.context should be(Some(<root/>))
+      chunk3.string should be("<three/>")
+      chunk3.context should be(Some(<root/>))
+    }
+    it("should inherit namespaces from parents") {
+      val c = XmlChunker() + "<root xmlns:a=\"urn:test\"><value a:name=\"Mario\"/><a:b/>"
+      c.hasChunks should be(true)
+      c.chunks.size should be(2)
+      val chunk1 :: chunk2 :: Nil = c.chunks
+      chunk1.string should be("<value a:name=\"Mario\"/>")
+      val a1 = chunk1.xml.get.attribute("urn:test", "name")
+      a1.isDefined should be(true)
+      a1.get.text should be("Mario")
+      chunk2.string should be("<a:b/>")
+      chunk2.xml.get.namespace should be("urn:test")
     }
 
     it("should also work with a depth of two (simple)") {
       val c = XmlChunker(2) + """<root><group id="a"><value>asdad</value><value/></group><group id="b"><name>Mario</name><age>29</age></group></root>"""
-      c.chunksAsString should be("<age>29</age>" :: "<name>Mario</name>" :: "<value/>" :: "<value>asdad</value>" :: Nil)
-      c.insideXml should be(false)
+      c.hasChunks should be(true)
+      c.chunks.size should be(4)
+      val chunk1 :: chunk2 :: chunk3 :: chunk4 :: Nil = c.chunks
+      chunk1.string should be("<value>asdad</value>")
+      chunk1.xml should be(Some(<value>asdad</value>))
+      chunk1.context should be(Some(<root><group id="a"/></root>))
+      chunk2.string should be("<value/>")
+      chunk2.xml should be(Some(<value/>))
+      chunk2.context should be(Some(<root><group id="a"/></root>))
+      chunk3.string should be("<name>Mario</name>")
+      chunk3.xml should be(Some(<name>Mario</name>))
+      chunk3.context should be(Some(<root><group id="b"/></root>))
+      chunk4.string should be("<age>29</age>")
+      chunk4.xml should be(Some(<age>29</age>))
+      chunk4.context should be(Some(<root><group id="b"/></root>))
     }
     it("should also work with a depth of two with a inline closed element") {
       val c = XmlChunker(2) + """<root><group id="a"><value>asdad</value><value/></group><group bla="bla"/><group id="b"><name>Mario</name><age>29</age></group></root>"""
-      c.chunksAsString should be("<age>29</age>" :: "<name>Mario</name>" :: "<value/>" :: "<value>asdad</value>" :: Nil)
-      c.insideXml should be(false)
+      c.hasChunks should be(true)
+      c.chunks.size should be(4)
+      val chunk1 :: chunk2 :: chunk3 :: chunk4 :: Nil = c.chunks
+      chunk1.string should be("<value>asdad</value>")
+      chunk1.xml should be(Some(<value>asdad</value>))
+      chunk1.context should be(Some(<root><group id="a"/></root>))
+      chunk2.string should be("<value/>")
+      chunk2.xml should be(Some(<value/>))
+      chunk2.context should be(Some(<root><group id="a"/></root>))
+      chunk3.string should be("<name>Mario</name>")
+      chunk3.xml should be(Some(<name>Mario</name>))
+      chunk3.context should be(Some(<root><group id="b"/></root>))
+      chunk4.string should be("<age>29</age>")
+      chunk4.xml should be(Some(<age>29</age>))
+      chunk4.context should be(Some(<root><group id="b"/></root>))
     }
     it("should have the second-level elements as roots with a depth of two") {
       val c = XmlChunker(2) + """<root><group id="a"><value>asdad</value><value/>"""
-      c.chunksAsString should be("<value/>" :: "<value>asdad</value>" :: Nil)
-      c.insideXml should be(true)
-      c.root should be(Some(<group id="a"/>))
+      c.hasChunks should be(true)
+      c.chunks.size should be(2)
+      val chunk1 :: chunk2 :: Nil = c.chunks
+      chunk1.string should be("<value>asdad</value>")
+      chunk1.xml should be(Some(<value>asdad</value>))
+      chunk1.context should be(Some(<root><group id="a"/></root>))
+      chunk2.string should be("<value/>")
+      chunk2.xml should be(Some(<value/>))
+      chunk2.context should be(Some(<root><group id="a"/></root>))
+    }
+    it("should support depth of three") {
+      val c = XmlChunker(3) + """<a><b><c><one/><two/></c><c2><three/>"""
+      c.hasChunks should be(true)
+      c.chunks.size should be(3)
+      val chunk1 :: chunk2 :: chunk3 :: Nil = c.chunks
+      chunk1.string should be("<one/>")
+      chunk1.xml should be(Some(<one/>))
+      chunk1.context should be(Some(<a><b><c/></b></a>))
+      chunk2.string should be("<two/>")
+      chunk2.xml should be(Some(<two/>))
+      chunk2.context should be(Some(<a><b><c/></b></a>))
+      chunk3.string should be("<three/>")
+      chunk3.xml should be(Some(<three/>))
+      chunk3.context should be(Some(<a><b><c2/></b></a>))
+    }
+
+    it("should work with a depth of zero") {
+      val c = XmlChunker(0) + """<a/><bla></bla><c a="b">hi</c>"""
+      c.hasChunks should be(true)
+      c.chunks.size should be(3)
+      val chunk1 :: chunk2 :: chunk3 :: Nil = c.chunks
+      chunk1.string should be("<a/>")
+      chunk1.context should be(None)
+      chunk2.string should be("<bla></bla>")
+      chunk2.context should be(None)
+      chunk3.string should be("<c a=\"b\">hi</c>")
+      chunk3.context should be(None)
     }
   }
 
