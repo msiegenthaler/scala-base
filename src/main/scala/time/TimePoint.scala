899fa7a3c279fd605dc3d0629f5c435893d88528
msiegenthaler <msiegenthaler@gmx.net>
2010-10-30 18:36:03 +0200

    add TimePoint class

---
+++ src/main/scala/time/TimePoint.scala
@@ -0,0 +1,47 @@
+package ch.inventsoft.scalabase
+package time
+
+
+/**
+ * A specific, absolute point in time (i.e. the 10:12:12.123 on 2nd of January 2010 (UTC))
+ * with millisecond precision.<br>
+ * Does not depend on timezones etc.
+ */
+trait TimePoint extends Ordered[TimePoint] with Equals {
+  /** ms (excl. leap seconds) sice 01.01.1970 00:00:00.000 UTF */
+  val unixTime: Long
+
+  def +(duration: Duration): TimePoint =
+    t(unixTime + duration.amountAs(Milliseconds))
+  def -(duration: Duration): TimePoint =
+    t(unixTime - duration.amountAs(Milliseconds))
+  def -(other: TimePoint): Duration =
+    (unixTime - other.unixTime) ms
+
+  override def compare(o: TimePoint) = {
+    longWrapper(unixTime).compareTo(o.unixTime)
+  }
+  override def hashCode = unixTime.hashCode
+  override def equals(o: Any) = o match {
+    case o: TimePoint =>
+      if (o.canEqual(this)) unixTime == o.unixTime
+      else false
+    case other => false
+  }
+  override def canEqual(o: Any) = o.isInstanceOf[TimePoint]
+
+  override def toString = {
+    new java.util.Date(unixTime).toString
+  }
+
+  private def t(t: Long) = TimePoint.unixTime(t)
+}
+
+object TimePoint {
+  def current: TimePoint = unixTime(System.currentTimeMillis())
+  def unixTime(time: Long): TimePoint = {
+    new TimePoint {
+      override val unixTime = time
+    }
+  }
+}
